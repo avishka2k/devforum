a9206bcf804b5056d50ff5845dcd0c166dfdda9b
avishka2k <avishka2k@gmail.com>
2024-05-19 13:50:02 +0530

    update format

--- server/src/post/post.controller.ts
+++ server/src/post/post.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { PostService } from './post.service';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { PosDto } from './dtos/post.dto';
 import { BlogPost } from './entities/post.entity';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @ApiBearerAuth('JWT-auth') 
 @Controller('post')
@@ -16,8 +17,20 @@ export class PostController {
           return this.postService.findAll();
      }
 
-     @Post(':id/')
+     @Get(':id')
+     findAllByUser(@Param('id') id: number) {
+          return this.postService.findAllByUser(id);
+     }
+
+     @UseGuards(AuthGuard)
+     @Post(':id')
      createPost(@Param('id') id: number, @Body() postDto: PosDto): Promise<BlogPost> {
           return this.postService.createPost(id, postDto);
+     }
+
+     @UseGuards(AuthGuard)
+     @Put(':postId')
+     updatePost(@Param('postId') postId: number, @Body() postDto: PosDto): Promise<BlogPost> {
+          return this.postService.updatePost(postId, postDto);
      }
 }
--- server/src/post/post.service.ts
+++ server/src/post/post.service.ts
@@ -16,6 +16,20 @@ export class PostService {
           return await this.postRepository.find();
      }
 
+     async findAllByUser(userId: number): Promise<BlogPost[]> {
+          const user = await this.userRepository.findOne({ where: { id: userId } });
+          const posts = await this.postRepository.find({ where: { user: { id: userId } } });
+          
+          if (!user) {
+               throw new NotFoundException('User not found');
+          }
+
+          if (!posts || posts.length === 0) {
+               throw new NotFoundException('Posts not found');
+          }
+          return posts;
+     }
+
      async createPost(userId: number, postDto: PosDto): Promise<BlogPost> {
           const user = await this.userRepository.findOne({ where: { id: userId } });
 
@@ -27,7 +41,18 @@ export class PostService {
                user,
                created_at: new Date(),
            });
-           delete user.password;
-          return await this.postRepository.save(post);
+          await this.postRepository.save(post);
+          delete user.password;
+          return post;
+     }
+
+     async updatePost(id: number, postDto: PosDto): Promise<BlogPost> {
+          const post = await this.postRepository.findOne({ where: { id } });
+
+          if (!post) {
+               throw new NotFoundException('Post not found');
+          }
+          await this.postRepository.update({ id }, postDto);
+          return await this.postRepository.findOne({ where: { id } });
      }
 }
