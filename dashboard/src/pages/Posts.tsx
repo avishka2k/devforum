b32257c9e0c873752c39932a1ee86591ba9de205
avishka2k <avishka2k@gmail.com>
2024-05-24 09:56:18 +0530

    initialize dashboard

---
+++ dashboard/src/pages/Posts.tsx
@@ -0,0 +1,16 @@
+import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
+import TableTwo from '../components/Tables/TableTwo';
+import DefaultLayout from '../layout/DefaultLayout';
+
+const Posts = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Posts" />
+      <div className="flex flex-col gap-10">
+        <TableTwo />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default Posts;
